392f33e8d5faa611683fd83f199c853e17288734
Mark Karpeles <magicaltux@gmail.com>
2019-11-01 21:22:58 +0900

    update script to generate .iso file

--- make-release.sh
+++ make-release.sh
@@ -6,13 +6,14 @@ INITVER=`readlink readlink /pkg/main/azusa.init | sed -r -e 's/.*\.([0-9]+)\..*/
 
 echo "Building initrd for kernel version $KVER with init $INITVER"
 
-RELEASE="$KVER.$INITVER"
+RELEASE="$(TZ=UTC date '+%Y%m%d%H%M%S')"
 INITRD_CPIO="initrd-$RELEASE.cpio"
 INITRD_DIR="initrd-$RELEASE-tmp"
-INITRD="initrd-$RELEASE.img"
-KERNEL="kernel-$RELEASE.img"
-ARCHIVE="azusa-$RELEASE.tar.gz"
-ROOTDIR="$PWD"
+INITRD="initrd-$KVER.img"
+KERNEL="kernel-$KVER.img"
+ARCHIVE="azusa-$RELEASE.iso"
+ROOTDIR="$PWD/cdroot"
+mkdir cdroot cdroot/isolinux
 
 echo "Preparing initrd for kernel $KVER"
 
@@ -37,23 +38,38 @@ cd "$ROOTDIR"
 rm -fr "$INITRD_DIR"
 
 echo "Compressing..."
-xz -v --check=crc32 --x86 --lzma2 --stdout "$INITRD_CPIO" >"$INITRD"
+xz -v --check=crc32 --x86 --lzma2 --stdout "$INITRD_CPIO" >"isolinux/$INITRD"
 rm -f "$INITRD_CPIO"
 
 echo "Copy kernel..."
-cp -T /pkg/main/sys-kernel.linux.core.$KVER/linux-$KVER.img "$KERNEL"
+cp -T /pkg/main/sys-kernel.linux.core.$KVER/linux-$KVER.img "isolinux/$KERNEL"
 
-echo "Archive..."
-tar czf "$ARCHIVE" "$INITRD" "$KERNEL"
+echo "Copy syslinux"
+cp -T /pkg/main/sys-boot.syslinux.core/share/syslinux/isolinux.bin isolinux/isolinux.bin
+cp -T /pkg/main/sys-boot.syslinux.core/share/syslinux/linux.c32 isolinux/linux.c32
+cp -T /pkg/main/sys-boot.syslinux.core/share/syslinux/ldlinux.c32 isolinux/ldlinux.c32
+cp -T /pkg/main/sys-boot.syslinux.core/share/syslinux/efi64/syslinux.efi isolinux/efiboot.img
+cat >isolinux/isolinux.cfg <<EOF
+DEFAULT azusa
+SAY Now booting AZUSA...
+
+LABEL azusa
+	KERNEL /isolinux/$KERNEL
+	APPEND initrd=/isolinux/$INITRD quiet loglevel=3 vga=current
+EOF
+
+echo "Make ISO..."
+cd ..
+xorriso -as mkisofs -o "$ARCHIVE" -isohybrid-mbr /pkg/main/sys-boot.syslinux.core/share/syslinux/isohdpfx.bin -c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e isolinux/efiboot.img -no-emul-boot -isohybrid-gpt-basdat cdroot
 
 echo "Checksum..."
 sha1sum -b "$ARCHIVE" >"release/azusa-$RELEASE.sha1"
 sha256sum -b "$ARCHIVE" >"release/azusa-$RELEASE.sha256"
 
-rm "$INITRD" "$KERNEL"
+rm -fr cdroot
 
 echo "Complete!"
-echo "git add release"
+echo "git add release/azusa-$RELEASE.*"
 echo "git commit -a -m 'Release $RELEASE'"
 echo "git tag v$RELEASE"
 echo "git push && git push --tags"
